0380c6c7ec10e26500705858720b1f6ddb8f64c6
Runmin Zhang <z.runmin@gmail.com>
2019-10-01 22:26:51 -0400

    add scripts

---
+++ init.sh
@@ -0,0 +1,6 @@
+# install gcc
+sudo apt install gcc 
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
---
+++ install_jupyter.sh
@@ -0,0 +1,5 @@
+jupyter notebook --generate-config
+jupyter notebook password
+
+conda install -c conda-forge nodejs
+jupyter labextension install jupyterlab_vim
